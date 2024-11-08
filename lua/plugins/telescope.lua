3b326c69eaefc33b11b090198a96a45c2d18cb17
hachoj <hjchojnowski@gmail.com>
2024-11-08 23:36:20 +0000

    fixed typo with the config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  condig = function() 
+  config = function() 
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
