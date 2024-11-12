0da2866f8eb29d313ee320cad1af4f173852374a
hachoj <hjchojnowski@gmail.com>
2024-11-12 10:17:49 -0500

    making sure up to date lol

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,87 +1,83 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "texlab", "ltex", "clangd" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { "williamboman/mason-lspconfig.nvim" },
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "texlab", "ltex", "clangd" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            local function get_python_path()
-                local conda_prefix = os.getenv("CONDA_PREFIX")
-                if conda_prefix then
-                    return conda_prefix .. "/bin/python"
-                else
-                    return vim.fn.exepath("python")
-                end
-            end
+			local function get_python_path()
+				local conda_prefix = os.getenv("CONDA_PREFIX")
+				if conda_prefix then
+					return conda_prefix .. "/bin/python"
+				else
+					return vim.fn.exepath("python")
+				end
+			end
 
-            lspconfig.pyright.setup({
-                before_init = function(_, config)
-                    config.settings.python.pythonPath = get_python_path()
-                end,
-                settings = {
-                    python = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            useLibraryCodeForTypes = true,
-                            diagnosticMode = "workspace",
-                        },
-                    },
-                },
-            })
+			lspconfig.pyright.setup({
+				before_init = function(_, config)
+					config.settings.python.pythonPath = get_python_path()
+				end,
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+						},
+					},
+				},
+			})
 
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.ltex.setup({
-                capabilities = capabilities,
-                settings = {
-                    ltex = {
-                        language = "en-US",
-                        latex = {
-                            enabled = true,
-                            commands = { "section", "cite", "ref" }, -- add common commands here
-                        },
-                    },
-                },
-            })
+			lspconfig.ltex.setup({
+				capabilities = capabilities,
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+			})
+
+			lspconfig.texlab.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -1,116 +1,120 @@
 return {
-	{
-		"quarto-dev/quarto-nvim",
-		dependencies = {
-			"jmbuhr/otter.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("quarto").setup()
-			-- Corrected keybinding for inserting a Python code block
-			vim.keymap.set({ "n", "i" }, "<C-M-i>", "<Esc>i```{python}<CR>```<Esc>O", { noremap = true, silent = true })
+    {
+        "quarto-dev/quarto-nvim",
+        dependencies = {
+            "jmbuhr/otter.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("quarto").setup()
+            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+                pattern = "*.qmd",
+                command = "set filetype=latex",
+            })
+            -- Corrected keybinding for inserting a Python code block
+            vim.keymap.set({ "n", "i" }, "<C-M-i>", "<Esc>i```{python}<CR>```<Esc>O", { noremap = true, silent = true })
 
-			-- Corrected OpenIPython function
-			vim.api.nvim_create_user_command("OpenIPython", function()
-				-- Open a new split below and move into it
-				vim.cmd("belowright split")
-				vim.cmd("resize 15") -- Optional: Adjust the terminal height
-				vim.cmd("term ipython") -- Open IPython in the terminal
+            -- Corrected OpenIPython function
+            vim.api.nvim_create_user_command("OpenIPython", function()
+                -- Open a new split below and move into it
+                vim.cmd("belowright split")
+                vim.cmd("resize 15") -- Optional: Adjust the terminal height
+                vim.cmd("term ipython") -- Open IPython in the terminal
 
-				-- Get the terminal buffer
-				local buf = vim.api.nvim_get_current_buf()
-				vim.api.nvim_buf_set_name(buf, "IPythonTerminal")
+                -- Get the terminal buffer
+                local buf = vim.api.nvim_get_current_buf()
+                vim.api.nvim_buf_set_name(buf, "IPythonTerminal")
 
-				-- Get the job ID
-				local job_id = vim.b.terminal_job_id
+                -- Get the job ID
+                local job_id = vim.b.terminal_job_id
 
-				if job_id then
-					vim.g.slime_default_config = { jobid = job_id }
-					print("Job ID set to:", job_id)
-				else
-					print("Failed to set Job ID: terminal_job_id not found")
-				end
+                if job_id then
+                    vim.g.slime_default_config = { jobid = job_id }
+                    print("Job ID set to:", job_id)
+                else
+                    print("Failed to set Job ID: terminal_job_id not found")
+                end
 
-				-- Return focus to the original window
-				vim.cmd("wincmd p")
-			end, {})
+                -- Return focus to the original window
+                vim.cmd("wincmd p")
+            end, {})
 
-			vim.keymap.set(
-				"n",
-				"<leader>ci",
-				":OpenIPython<CR>",
-				{ noremap = true, silent = true, desc = "Open IPython terminal split" }
-			)
-		end,
-	},
+            vim.keymap.set(
+                "n",
+                "<leader>ci",
+                ":OpenIPython<CR>",
+                { noremap = true, silent = true, desc = "Open IPython terminal split" }
+            )
+        end,
+    },
 
-	{
-		"jpalardy/vim-slime",
-		init = function()
-			vim.g.slime_target = "neovim"
-			vim.g.slime_python_ipython = 1
-			vim.g.slime_dispatch_ipython_pause = 100
-			vim.g.slime_cell_delimiter = "# %%"
+    {
+        "jpalardy/vim-slime",
+        init = function()
+            vim.g.slime_target = "neovim"
+            vim.g.slime_python_ipython = 1
+            vim.g.slime_dispatch_ipython_pause = 100
+            vim.g.slime_cell_delimiter = "# %%"
 
-			-- Corrected send_python_block function
-			local function send_python_block()
-				-- Get the current cursor position
-				local cursor_line = vim.api.nvim_win_get_cursor(0)[1] - 1 -- 0-based indexing
+            -- Corrected send_python_block function
+            local function send_python_block()
+                -- Get the current cursor position
+                local cursor_line = vim.api.nvim_win_get_cursor(0)[1] - 1 -- 0-based indexing
 
-				local bufnr = vim.api.nvim_get_current_buf()
-				local total_lines = vim.api.nvim_buf_line_count(bufnr)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local total_lines = vim.api.nvim_buf_line_count(bufnr)
 
-				local start_line = nil
-				local end_line = nil
+                local start_line = nil
+                local end_line = nil
 
-				-- Scan upwards to find ```{python}
-				for i = cursor_line, 0, -1 do
-					local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
-					if line:match("^```{python}") then
-						start_line = i + 1 -- Exclude the ```{python} line
-						break
-					end
-				end
+                -- Scan upwards to find ```{python}
+                for i = cursor_line, 0, -1 do
+                    local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
+                    if line:match("^```{python}") then
+                        start_line = i + 1 -- Exclude the ```{python} line
+                        break
+                    end
+                end
 
-				if not start_line then
-					print("No starting code block delimiter found.")
-					return
-				end
+                if not start_line then
+                    print("No starting code block delimiter found.")
+                    return
+                end
 
-				-- Scan downwards to find ```
-				for i = cursor_line, total_lines - 1 do
-					local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
-					if line:match("^```$") then
-						end_line = i - 1 -- Exclude the ``` line
-						break
-					end
-				end
+                -- Scan downwards to find ```
+                for i = cursor_line, total_lines - 1 do
+                    local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
+                    if line:match("^```$") then
+                        end_line = i - 1 -- Exclude the ``` line
+                        break
+                    end
+                end
 
-				if not end_line then
-					print("No ending code block delimiter found.")
-					return
-				end
+                if not end_line then
+                    print("No ending code block delimiter found.")
+                    return
+                end
 
-				if start_line > end_line then
-					print("Invalid code block boundaries.")
-					return
-				end
+                if start_line > end_line then
+                    print("Invalid code block boundaries.")
+                    return
+                end
 
-				-- Extract code between start_line and end_line
-				local code_lines = vim.api.nvim_buf_get_lines(bufnr, start_line, end_line + 1, false)
+                -- Extract code between start_line and end_line
+                local code_lines = vim.api.nvim_buf_get_lines(bufnr, start_line, end_line + 1, false)
 
-				-- Send each line using slime#send
-				for _, line in ipairs(code_lines) do
-					vim.fn["slime#send"](line .. "\n")
-				end
-				-- Send an extra newline to execute the code block
-				vim.fn["slime#send"]("\n")
+                -- Send each line using slime#send
+                for _, line in ipairs(code_lines) do
+                    vim.fn["slime#send"](line .. "\n")
+                end
+                -- Send an extra newline to execute the code block
+                vim.fn["slime#send"]("\n")
 
-				print("Sent Python code block from lines", start_line + 1, "to", end_line + 1)
-			end
+                print("Sent Python code block from lines", start_line + 1, "to", end_line + 1)
+            end
 
-			-- Keybinding to send the Python code block to IPython terminal
-			vim.keymap.set("n", "<leader>cs", send_python_block, { noremap = true, silent = true })
-		end,
-	},
+            -- Keybinding to send the Python code block to IPython terminal
+            vim.keymap.set("n", "<leader>cs", send_python_block, { noremap = true, silent = true })
+        end,
+    },
 }
